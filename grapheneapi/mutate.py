aaae14f9f63b34e11762e0b629883106ccecb319
marse11e <endless.key.2018@gmail.com>
2024-05-06 16:26:21 +0500

    Graphene

--- grapheneapi/mutate.py
+++ grapheneapi/mutate.py
@@ -7,15 +7,26 @@ from grapheneapi.modeltype import BrandModelType, ClothesModelType
 
 class CreateBrand(graphene.Mutation):
     class Arguments:
+        # Аргументы которую далжен заполнить пользователь для создания бренда
+        # required=True озночает что данное поле должно быть 
+        # заполнено обязательно
         name = graphene.String(required=True)
         country = graphene.String(required=True)
 
+    # Тут мы предолтавляем пользователю выбрать какие поля должны показываться
+    # после создания 
     brand = graphene.Field(BrandModelType)
 
+    # Тут мы делаем действие и функция mutate всегда с перва делжна принемать
+    # такие параметры как self, info дальше уже в зависимосьти от аргументов
     def mutate(self, info, name, country):
+        # Тут мы создаем бренд только говорил name=name и country=country 
+        # в примере name='Qazaq Republic' а вот country='KZ'
         brand = Brand.objects.create(
             name=name, country=country
         )
+
+        # Тут вы возвыращаем то что выпрал.
         return CreateBrand(brand=brand)
 
 
